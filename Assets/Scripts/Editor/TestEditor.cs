5c719a9fa33cb84140a75f734c1209748d01ffaa
CoreyD177 <corey.david87@live.com>
2022-07-31 20:46:18 +1000

    Updated some variables and commented code

--- Assets/Scripts/Editor/TestEditor.cs
+++ Assets/Scripts/Editor/TestEditor.cs
@@ -3,91 +3,104 @@ using UnityEditor;
 
 namespace TestEditor
 {
+    //Change the way any short variables are displayed
     [CustomPropertyDrawer(typeof(short))]
     public class TestEditor : PropertyDrawer
     {
-        private GUIStyle _editorStyle;
+        //Create an array of colours to use to style the properties
         Color32[] colors = new Color32[] { Color.red, Color.blue };
-
+        //create a style variable to modify the look of the editor properties
+        private GUIStyle _editorStyle;
+        
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
+            //If GUI style doesn't contain anything make it equal the Unity default style from Pane Options
             if (_editorStyle == null)
             {
                 _editorStyle = new GUIStyle(GUI.skin.GetStyle("PaneOptions"));
                 _editorStyle.imagePosition = ImagePosition.ImageOnly;
             }
-
+            //Begin and create a label for the property
             label = EditorGUI.BeginProperty(position, label, property);
-
-            property.intValue = Mathf.Clamp(property.intValue, 10, 200);
+            //Make sure the value of the property sits in the desired range
+            property.intValue = Mathf.Clamp(property.intValue, 10, 100);
+            //If name of property contains player colour it blue
             if (property.name.ToString().Contains("player"))
             {
                 GUI.backgroundColor = colors[1];
             }
+            //If name of property contains enemy colour it red
             else if (property.name.ToString().Contains("enemy"))
             {
                 GUI.backgroundColor = colors[0];
             }
-
-            EditorGUI.IntSlider(position, property, 10, 200);
-
+            //Create a slider to adjust the value of the property
+            EditorGUI.IntSlider(position, property, 10, 100);
+            //Indent the property
             int _indent = EditorGUI.indentLevel;
             EditorGUI.indentLevel = 0;
-
             EditorGUI.indentLevel = _indent;
-            EditorGUI.EndProperty();
-            
+            //End the property field
+            EditorGUI.EndProperty();            
         }
     }
 
+    //Change the way GameObject variables are displayed
     [CustomPropertyDrawer(typeof(GameObject))]
-
     public class CustomGameObject : PropertyDrawer
     {
+        //Create an array of colours to use to style the properties
+        Color32[] colors = new Color32[] { Color.red, Color.blue, Color.green };
+        //create a style variable to modify the look of the editor properties
         private GUIStyle _editorStyle;
-        Color32[] colors = new Color32[] { Color.red, Color.blue };
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
+            //If editor style is empty make it the default unity editor style
             if (_editorStyle == null)
             {
                 _editorStyle = new GUIStyle(GUI.skin.GetStyle("PaneOptions"));
                 _editorStyle.imagePosition = ImagePosition.ImageOnly;
             }
-
+            //Begin and label the property
             label = EditorGUI.BeginProperty(position, label, property);
             position = EditorGUI.PrefixLabel(position, label);
-
+            //If property name contains player colour it blue
             if (property.name.ToString().Contains("player"))
             {
                 GUI.backgroundColor = colors[1];
             }
+            //If property name contains enemy colour it red
             else if (property.name.ToString().Contains("enemy"))
             {
                 GUI.backgroundColor = colors[0];
             }
+            //If property name contains plane colour it green
             else if (property.name.ToString().Contains("plane"))
             {
-                GUI.backgroundColor = Color.green;
+                GUI.backgroundColor = colors[2];
             }
-
+            //Create the box to add the gameobject in the editor
             EditorGUI.PropertyField(position, property, GUIContent.none);
-
+            //Indent the property field
             int _indent = EditorGUI.indentLevel;
             EditorGUI.indentLevel = 0;
-
             EditorGUI.indentLevel = _indent;
+            //End the property
             EditorGUI.EndProperty();
         }
     }
-
-    [CustomEditor(typeof(TestProperties))]
+    //Custom Editor to add the button for playmode tint
+    [CustomEditor(typeof(ModifiedTestCode))]
     public class PlaymodeTint : Editor
     {
         public override void OnInspectorGUI()
         {
+            //Draw the default editor properties for the script first
             DrawDefaultInspector();
-            var myScript = target as TestProperties;
+            //Create a reference to the spawning test script so we can access a function
+            var myScript = target as ModifiedTestCode;
+            //If user clicks the button activate the ChangeTint method so playmode tint changes
             if (GUILayout.Button("Change Tint"))
             {
                 myScript.ChangeTint();
--- Assets/Scripts/Spawning/ModifiedTestCode.cs
+++ Assets/Scripts/Spawning/ModifiedTestCode.cs
@@ -1,28 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
 public class ModifiedTestCode : MonoBehaviour
 {
+    //GameObjects to spawn
     public GameObject plane;
     public GameObject playerPrefab;
     public GameObject enemyPrefab;
+    //Amount of units to spawn, will adjust spawning area based on amount of units
     public short playerCount;
     public short enemyCount;
 
     private void Start()
     {
+        //Spawn the plane according to the SpawnPlane function from the SpawnCode script
         SpawnCode.SpawnPlane(plane, 10);
+        //Use the SpawnMixed function of the SpawnCode script to spawn units based on the amount and spread set by playerCount
         for (int i = 0; i < playerCount; i++)
         {
-            SpawnCode.SpawnMixed(playerPrefab, 10);
+            SpawnCode.SpawnMixed(playerPrefab, playerCount * 0.5f);
         }
+        //Use the SpawnMixed function of the SpawnCode script to spawn units based on the amount and spread set by enemyCount
         for (int i = 0; i < enemyCount; i++)
         {
-            SpawnCode.SpawnMixed(enemyPrefab, 10);
+            SpawnCode.SpawnMixed(enemyPrefab, enemyCount * 0.5f);
         }
     }
+    //Create function to change the colour of playmode tint. Currently set to change to Red
     public void ChangeTint()
     {
         EditorPrefs.SetString("Playmode tint", "Playmode tint;1;0;0;1");
